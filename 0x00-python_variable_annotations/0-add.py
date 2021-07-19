43012380926328d0d835a1e159012cf0f0283fb3
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2021-07-19 11:24:41 -0500

    ADD Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" function that return sum """
+
+
+def add(a: float, b: float) -> float:
+    """
+    Funtion that sum two float numbers
+    args:
+        a: float number
+        b: float number
+    Return: sum
+    """
+    return a + b
